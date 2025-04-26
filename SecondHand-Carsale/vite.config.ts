00ad82a2baa5e6a5f96778c008c866e5be14020e
VIJAYANAARAAYANAN <vijaybalaram05@gmail.com>
2025-04-26 13:51:05 +0530

    updatedConnection

--- SecondHand-Carsale/vite.config.ts
+++ SecondHand-Carsale/vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,   // <-- this allows connections from anywhere (0.0.0.0)
     port: 5173,
-    strictPort: true, // force using 5173 exactly, not changing if unavailable
-  },
+    strictPort: true,
+    allowedHosts: [
+      'dockertest-carsaele.onrender.com' 
+    ]
+  }
 })
